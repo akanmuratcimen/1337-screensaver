9178eee1df7fedb94f58f31f60c4462e80e6f6e2
akanmuratcimen <akanmuratcimen@debian-BULLSEYE-live-builder-AMD64>
2022-05-22 18:01:01 +0000

    sample-1 done.

--- sample-1/main.c
+++ sample-1/main.c
@@ -3,47 +3,49 @@
 int main(
   void
 ) {
-  const int screenWidth = GetScreenWidth();
-  const int screenHeight = GetScreenHeight();
+  int screenWidth = GetScreenWidth();
+  int screenHeight = GetScreenHeight();
 
   const Color bgColor = { 12, 12, 245, 255 };
-  const Color fontColor = { 5, 226, 115, 180 };
+  const Color fontColor = { 5, 226, 115, 255 };
+  const Color clockBgColor = { 12, 12, 245, 130 };
+  const Color clockBorderColor = { 5, 226, 115, 180  };
+
+  //SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
   InitWindow(screenWidth, screenHeight, "1337 Screensaver");
 
   ToggleFullscreen();
 
+  const int bgFontSize = 190;
   const char *bgText = "133713371337133713371337133713371337133713371337133713371337";
-  Font font = LoadFontEx("sora.ttf", 96, 0, 0);
-  Image bgTextImage = ImageTextEx(font, bgText, 72, 0, fontColor);
+
+  Font bgFont = LoadFontEx("sora.ttf", bgFontSize, 0, 0);
+  Image bgTextImage = ImageTextEx(bgFont, bgText, bgFontSize, 0, fontColor);
   Texture2D bgTexture = LoadTextureFromImage(bgTextImage);
+
   UnloadImage(bgTextImage);
 
   SetTargetFPS(60);
 
   const int line_height = 140;
 
-  int scrollingBack = 0;
+  float scrollingBack = 0;
 
   while (!WindowShouldClose()) {
-    scrollingBack -= 1.0;
+    scrollingBack -= 0.3f;
 
-    if (scrollingBack <= -bgTexture.width * 2) {
+    if (scrollingBack <= -bgTexture.width) {
       scrollingBack = 0;
     }
 
     BeginDrawing();
 
       ClearBackground(bgColor);
-      
+
       for (int i = 0; i < 24; i++) {
         const Vector2 bg1Pos = {
           scrollingBack - 64 * i,
-          i * line_height - 24
-        };
-
-        const Vector2 bg2Pos = {
-          bgTexture.width * 2 + scrollingBack - (64 * i),
           i * line_height - 24
         };
 
@@ -51,15 +53,20 @@ int main(
           bgTexture,
           bg1Pos,
           0.0f,
-          2.0f,
+          1.0f,
           WHITE
         );
+
+        const Vector2 bg2Pos = {
+          bgTexture.width + scrollingBack - (64 * i),
+          i * line_height - 24
+        };
 
         DrawTextureEx(
           bgTexture,
           bg2Pos,
           0.0f,
-          2.0f,
+          1.0f,
           WHITE
         );
       }
@@ -67,7 +74,7 @@ int main(
     EndDrawing();
   }
 
-  UnloadFont(font);
+  UnloadFont(bgFont);
   UnloadTexture(bgTexture);
 
   CloseWindow();
