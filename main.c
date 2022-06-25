d87c3729da1726e7aef5dd1693a5ae441c43d6de
akanmuratcimen <akanmuratcimen@gmail.com>
2022-06-25 02:19:23 +0200

    refactoring

--- main.c
+++ main.c
@@ -8,8 +8,14 @@
 #include <GLFW/glfw3.h>
 #include <cglm/cglm.h>
 
-#include "core.h"
-#include "draw.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "shaders/char_vs.h"
+#include "shaders/char_fs.h"
+
+#include "shaders/rect_vs.h"
+#include "shaders/rect_fs.h"
 
 #define FULLSCREEN 1
 #define EXIT_ON_INPUT 1
@@ -24,6 +30,50 @@
 extern "C" {
 #endif
 
+struct texture {
+  unsigned int id;
+  int width;
+  int height;
+};
+
+struct texture_offset {
+  float x;
+  float width;
+};
+
+struct texture
+load_texture(
+  const char *file_name
+);
+
+struct vector2 {
+  float x;
+  float y;
+};
+
+struct cell {
+  char value;
+  int column;
+  int row;
+  struct vector2 position;
+  float pause_position;
+  bool is_highlighting;
+  bool is_highlighted;
+};
+
+struct column {
+  float speed;
+  bool is_highlighting;
+  float highlighting_time_start;
+  float top_position;
+  int highlighting_row_index;
+};
+
+struct window_size {
+  int width;
+  int height;
+} window_size;
+
 const char chars[] = { '1', '3', '7' };
 
 float line_height;
@@ -34,8 +84,6 @@ float screen_offset_x;
 float char_width;
 char *texture_name;
 int speed;
-
-struct window_size window_size;
 
 GLFWwindow *window = NULL;
 
@@ -108,6 +156,25 @@ create_window(
 void
 init_scaling(
   const int screen_width
+);
+
+void
+draw_rectangle(
+  const float x,
+  const float y,
+  const float w,
+  const float h,
+  struct window_size window_size
+);
+
+void
+draw_bulk(
+  const int column_count,
+  const int row_count,
+  struct cell cells[column_count][row_count],
+  struct window_size window_size,
+  const float cell_size,
+  const char *texture_name
 );
 
 int
@@ -622,6 +689,384 @@ init_scaling(
   match_threshold = char_width * 0.5f;
   is_aligned_threshold = char_width * 0.1f;
   screen_offset_x = char_width * 0.4f * -1.0f;
+}
+
+void
+show_gl_shader_compilation_error(
+  const GLuint shaderHandle
+) {
+  int errorLogLength;
+
+  glGetShaderiv(shaderHandle, GL_INFO_LOG_LENGTH, &errorLogLength);
+  char *buffer = (char *) malloc(errorLogLength + 1);
+
+  glGetShaderInfoLog(shaderHandle, errorLogLength + 1, NULL, buffer);
+  fprintf(stderr, buffer);
+
+  free(buffer);
+}
+
+GLuint
+load_shader(
+  unsigned char *buffer,
+  const GLenum shaderType
+) {
+  int compilationStatus;
+
+  GLuint shader = glCreateShader(shaderType);
+
+  glShaderSource(shader, 1, (const char **) &buffer, NULL);
+  glCompileShader(shader);
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compilationStatus);
+
+  if (compilationStatus != GL_TRUE) {
+    fprintf(stderr, "VERTEX SHADER ERROR\n");
+    show_gl_shader_compilation_error(shader);
+  }
+
+  return shader;
+}
+
+GLuint
+compile_shaders(
+  unsigned char *vs_file_buffer,
+  unsigned char *fs_file_buffer
+) {
+  GLuint program = glCreateProgram();
+
+  GLuint vs = load_shader(vs_file_buffer, GL_VERTEX_SHADER);
+  GLuint fs = load_shader(fs_file_buffer, GL_FRAGMENT_SHADER);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+
+  glLinkProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+}
+
+static bool rect_shader_loaded = false;
+GLuint rect_shader_id;
+
+void
+draw_rectangle(
+  const float x,
+  const float y,
+  const float w,
+  const float h,
+  struct window_size window_size
+) {
+  if (!rect_shader_loaded) {
+    rect_shader_id = compile_shaders(
+      shaders_rect_vs_glsl,
+      shaders_rect_fs_glsl
+    );
+
+    rect_shader_loaded = true;
+  }
+
+  glUseProgram(rect_shader_id);
+
+  mat4 transform;
+  glm_mat4_identity(transform);
+
+  glm_ortho(
+    0.0f,
+    window_size.width,
+    window_size.height,
+    0.0f,
+    -1.0f,
+    1.0f,
+    transform
+  );
+
+  glUniformMatrix4fv(1, 1, GL_FALSE, transform[0]);
+
+  float vertices[] = {
+    x, y,
+    x + w, y + h,
+    x, y + h,
+
+    x, y,
+    x + w, y + h,
+    x + w, y
+  };
+
+  unsigned int VAO, VBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    sizeof(vertices),
+    vertices,
+    GL_STATIC_DRAW
+  );
+
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(
+    0,
+    2,
+    GL_FLOAT,
+    GL_FALSE,
+    2 * sizeof(float),
+    (void *) 0
+  );
+
+  glBindVertexArray(VAO);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+  glBindVertexArray(0);
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+}
+
+#include "textures/sprite_1k.h"
+#include "textures/sprite_2k.h"
+#include "textures/sprite_4k.h"
+
+struct texture
+load_texture(
+  const char *texture_name
+) {
+  unsigned char *texture_data = NULL;
+  unsigned int texture_data_len = 0;
+
+  if (strcmp(texture_name, "1k") == 0) {
+    texture_data = textures_sprite_1k_png;
+    texture_data_len = textures_sprite_1k_png_len;
+  } else if (strcmp(texture_name, "2k") == 0) {
+    texture_data = textures_sprite_2k_png;
+    texture_data_len = textures_sprite_2k_png_len;
+  } else if (strcmp(texture_name, "4k") == 0) {
+    texture_data = textures_sprite_4k_png;
+    texture_data_len = textures_sprite_4k_png_len;
+  }
+
+  int width;
+  int height;
+  int channels;
+
+  unsigned char *data =
+    stbi_load_from_memory(
+      texture_data,
+      texture_data_len,
+      &width,
+      &height,
+      &channels,
+      0
+    );
+
+  unsigned int id;
+
+  glGenTextures(1, &id);
+  glBindTexture(GL_TEXTURE_2D, id);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  glTexImage2D(
+    GL_TEXTURE_2D,
+    0,
+    GL_RGBA,
+    width,
+    height,
+    0,
+    GL_RGBA,
+    GL_UNSIGNED_BYTE,
+    data
+  );
+
+  glEnable( GL_BLEND );
+  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  struct texture texture = {
+    .id = id,
+    .width = width,
+    .height = height,
+  };
+
+  stbi_image_free(data);
+
+  return texture;
+}
+
+struct texture texture;
+static bool texture_loaded = false;
+
+static bool char_shader_loaded = false;
+GLuint char_shader_id;
+
+void
+draw_bulk(
+  const int column_count,
+  const int row_count,
+  struct cell cells[column_count][row_count],
+  struct window_size window_size,
+  const float cell_size,
+  const char *texture_file_name
+) {
+  if (!texture_loaded) {
+    texture = load_texture(texture_file_name);
+    texture_loaded = true;
+  }
+
+  if (!char_shader_loaded) {
+    char_shader_id = compile_shaders(
+      shaders_char_vs_glsl,
+      shaders_char_fs_glsl
+    );
+
+    char_shader_loaded = true;
+  }
+
+  glUseProgram(char_shader_id);
+
+  mat4 transform;
+  glm_mat4_identity(transform);
+
+  glm_ortho(
+    0.0f,
+    window_size.width,
+    window_size.height,
+    0.0f,
+    -1.0f,
+    1.0f,
+    transform
+  );
+
+  glUniformMatrix4fv(4, 1, GL_FALSE, transform[0]);
+
+  float v[column_count * row_count][4];
+
+  int i = 0;
+  for (int column = 0; column < column_count; column++) {
+    for (int row = 0; row < row_count; row++, i++) {
+      struct cell cell = cells[column][row];
+
+      v[i][0] = cell.position.x;
+      v[i][1] = cell.position.y;
+
+      int tex_mul = 0;
+
+      switch (cell.value) {
+        case '1': tex_mul = 0; break;
+        case '3': tex_mul = 1; break;
+        case '7': tex_mul = 2; break;
+      }
+
+      v[i][2] = tex_mul * cell_size / texture.width;
+      v[i][3] = 0.0f;
+    }
+  }
+
+  unsigned int iVBO;
+
+  glGenBuffers(1, &iVBO);
+  glBindBuffer(GL_ARRAY_BUFFER, iVBO);
+
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    sizeof(float) * 4 * column_count * row_count,
+    &v,
+    GL_STATIC_DRAW
+  );
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  float tx = cell_size / texture.width;
+  float ty = cell_size / texture.height;
+
+  float vertices[] = {
+    0.0f, cell_size,       0.0f, ty,   // bottom left
+    cell_size, 0.0f,       tx, 0.0f,   // top right
+    0.0f, 0.0f,            0.0f, 0.0f, // top left
+
+    0.0f, cell_size,       0.0f, ty,   // bottom left
+    cell_size, 0.0f,       tx, 0.0f,   // top right
+    cell_size, cell_size,  tx, ty      // bottom right
+  };
+
+  unsigned int VAO, VBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    sizeof(vertices),
+    vertices,
+    GL_STATIC_DRAW
+  );
+
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(
+    0,
+    2,
+    GL_FLOAT,
+    GL_FALSE,
+    4 * sizeof(float),
+    (void *) 0
+  );
+
+  glEnableVertexAttribArray(2);
+
+  glVertexAttribPointer(
+    2,
+    2,
+    GL_FLOAT,
+    GL_FALSE,
+    4 * sizeof(float),
+    (void *) (2 * sizeof(float))
+  );
+
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, iVBO);
+
+  glVertexAttribPointer(
+    1,
+    2,
+    GL_FLOAT,
+    GL_FALSE,
+    4 * sizeof(float),
+    (void *) 0
+  );
+
+  glVertexAttribDivisor(1, 1);
+
+  glEnableVertexAttribArray(3);
+
+  glVertexAttribPointer(
+    3,
+    2,
+    GL_FLOAT,
+    GL_FALSE,
+    4 * sizeof(float),
+    (void *) (2 * sizeof(float))
+  );
+
+  glVertexAttribDivisor(3, 1);
+
+  glBindVertexArray(VAO);
+  glDrawArraysInstanced(GL_TRIANGLES, 0, 6, column_count * row_count);
+  glBindVertexArray(0);
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &iVBO);
 }
 
 #if defined(__cplusplus)
--- makefile
+++ makefile
@@ -3,8 +3,7 @@ CFLAGS := -Wall -Wextra -std=gnu11 -pedantic -O3 -fdiagnostics-color=always
 LIBSX11 := -lGL -lm -lpthread -ldl -lrt -lX11 -lglut -lGLEW -lglfw -lglfw3 -I cglm/include -L cglm/build -lcglm
 LIBSW32 := -lopengl32 -lgdi32 -lwinmm -mwindows -lwinpthread --static
 OUTPUT := 1337
-SRC := main.c draw.c
-
+SRC := main.c
 
 linux:
 	$(CC) $(SRC) $(CFLAGS) $(LIBSX11) -o $(OUTPUT)
